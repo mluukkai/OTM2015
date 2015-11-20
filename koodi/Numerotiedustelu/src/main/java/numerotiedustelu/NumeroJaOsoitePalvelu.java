9ec92ebb98a24ea5f1184468133c4c666e809f6b
Matti Luukkainen <mluukkai@iki.fi>
2015-11-20 22:39:49 +0200

    koodia vkolle 5

---
+++ koodi/Numerotiedustelu/src/main/java/numerotiedustelu/NumeroJaOsoitePalvelu.java
@@ -0,0 +1,36 @@
+package numerotiedustelu;
+ 
+import java.util.*;
+ 
+public class NumeroJaOsoitePalvelu {
+ 
+    public NumeroJaOsoitePalvelu() {
+    }
+ 
+    public void lisaaNumero(String nimi, String numero) {
+
+    }
+ 
+    public Collection<String> haeNumerot(String nimi) {
+        return null;
+    }
+ 
+    public String haeNimi(String numero) {
+        return null;
+    }
+ 
+    public void lisaaOsoite(String nimi, String katu, String kaupunki) {
+
+    }
+ 
+    public Henkilo haeNimenPerusteella(String nimi) {
+
+        return null;
+    }
+ 
+ 
+    public void poista(String nimi) {
+
+    }
+ 
+}
